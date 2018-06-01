1658a5e8cc357ec15e51cdc76fbd5a76de6b7e9c
Florian Hübsch <flo@pace.car>
2018-06-01 11:16:35 +0200

    Add themes and build script

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+jq --stream 'select(.[1] != null) | flatten | join(".") | "--\(.)" | sub("\\.(?<x>[^.]+)$"; "=\(.x)")' themes/pace.json  | xargs redoc-cli bundle -o $1 $2 -t themes/pace.hbs
